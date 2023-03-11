94a21017c1fd2f562facc58e06ccaafb79c3079b
denuden <79180351+denuden@users.noreply.github.com>
2023-03-11 13:13:49 +0800

    reports and dashboard

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,56 +1,73 @@
-import {AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
 import { MaterialModules } from 'src/app/modules/material.module';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ViewQuestionComponent } from '../view-question/view-question.component';
 import { ViewPostComponent } from 'src/app/main/components/view-post/view-post.component';
 
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements AfterViewInit {
-  displayedColumns: string[] = ['id', 'student_name', 'student_program',  'title', 'description', 'created_at', 'action'];
+  displayedColumns: string[] = [
+    'id',
+    'student_name',
+    'student_program',
+    'title',
+    'description',
+    'created_at',
+    'action',
+  ];
   dataSource!: MatTableDataSource<any>;
-
+  postscount: number = 0;
+  userscount: number = 0;
+  commentscount: number = 0;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  posts$: Array<any> =[];
+  posts$: Array<any> = [];
 
   constructor(
     public _apiService: AuthService,
     private dialog: MatDialog,
-    private route: Router,
+    private route: Router
   ) {
-    this._apiService.request('showAllGlobal', '', this.posts$, 'get').subscribe((res:any)=>{
-      this.dataSource = new MatTableDataSource(res);
+    this._apiService
+      .request('showAllGlobal', '', this.posts$, 'get')
+      .subscribe((res: any) => {
+        this.dataSource = new MatTableDataSource(res);
 
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-      this.posts$ = res;
-      console.log(res);
-
-    }), (error: any)=>{
-      alert("Error posting data...");
-      console.log("Error posting data", error);
-    }
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.posts$ = res;
+        console.log(res);
+      }),
+      (error: any) => {
+        alert('Error posting data...');
+        console.log('Error posting data', error);
+      };
+    this._apiService
+      .request('adminDashboard', '', '', 'get')
+      .subscribe((res: any) => {
+        this.userscount = res.userscount;
+        this.commentscount = res.commentscount;
+        this.postscount = res.postscount;
+      }),
+      (error: any) => {
+        alert('Error getting data...');
+        console.log('Error getting data', error);
+      };
   }
 
-  ngAfterViewInit() {
+  ngAfterViewInit() {}
 
-  }
-
-  ngOnInit(){
-
-  }
-
+  ngOnInit() {}
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -61,30 +78,19 @@ export class DashboardComponent implements AfterViewInit {
     }
   }
 
-
-  viewPost(id: number): void{
+  viewPost(id: number): void {
     this.route.navigateByUrl('main/view-post/' + id);
-
   }
 
-  viewStudentPost(id: number): void{
+  viewStudentPost(id: number): void {
     this.dialog.open(ViewPostComponent, {
-
       width: '100vh',
       height: '100vh',
       maxWidth: '100vw',
       maxHeight: '100vh',
-      data:{
-        id: id
-      }
-
-  });
-
+      data: {
+        id: id,
+      },
+    });
   }
-
 }
-
-
-
-
-
--- src/app/admin/components/reports/reports.component.ts
+++ src/app/admin/components/reports/reports.component.ts
@@ -1,10 +1,15 @@
-import { Component, OnInit , ViewChild } from '@angular/core';
-import { FormControl,FormGroup,Validators,FormBuilder } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthService } from 'src/app/services/auth.service';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { RegStudentComponent } from '../reg-student/reg-student.component';
@@ -12,27 +17,44 @@ import { RegStudentComponent } from '../reg-student/reg-student.component';
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.scss']
+  styleUrls: ['./reports.component.scss'],
 })
 export class ReportsComponent implements OnInit {
-
-  displayedColumns: string[] = ['no.', 'user_id', 'post_id', 'report', 'created_at', 'action'];
+  displayedColumns: string[] = [
+    'id',
+    'user_id',
+    'post_id',
+    'report',
+    'created_at',
+    'action',
+  ];
   dataSource!: MatTableDataSource<any>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
-
     public _apiService: AuthService,
     private route: Router,
     public snackbar: MatSnackBar,
-    private dialog: MatDialog,
-  ) { }
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
-  }
+    this._apiService
+      .request('showAllReports', '', '', 'get')
+      .subscribe((res: any) => {
+        this.dataSource = new MatTableDataSource(res);
 
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        console.log(res);
+      }),
+      (error: any) => {
+        alert('Error posting data...');
+        console.log('Error posting data', error);
+      };
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -42,5 +64,4 @@ export class ReportsComponent implements OnInit {
       this.dataSource.paginator.firstPage();
     }
   }
-
 }
